fbd4717bc4f44c0ef3a257d3344a666eb7124e3f
heebp <skdnfl4343@gmail.com>
2024-11-14 09:33:53 +0900

    feat: constant 탭 정의

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
 import BottomTab from "@/components/BottomTab";
+import { OutletContainer } from "./Default.styles";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setCurrentTab } from "@/states/tabSlice";
+import { useLocation } from "react-router-dom";
 
 export default function DefaultLayout() {
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        dispatch(setCurrentTab(window.location.pathname));
+    }, [dispatch, location.pathname]);
+
     return (
         <>
             <Header />
-            <Outlet />
+            <OutletContainer>
+                <Outlet />
+            </OutletContainer>
             <BottomTab />
         </>
     );
--- src/pages/Home.styles.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Root = styled.div`
-    margin-top: 44px;
-    height: 100vh;
-`;
