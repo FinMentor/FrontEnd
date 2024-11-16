efcbd4f03599abfba4b6d3a948ec09875b7f4129
heebp <skdnfl4343@gmail.com>
2024-11-17 01:27:39 +0900

    refactor: 헤더 뒤로가기 버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,33 @@
-import { Nav, NavLink } from "./Header.styles";
-import { useSelector, useDispatch } from "react-redux";
-import { exitChatRoom } from "../states/chatSlice";
-import { setShowNavigator } from "../states/navigatorSlice";
+import { Nav } from "./Header.styles";
+import { useLocation, useNavigate } from "react-router-dom";
+import BackArrowIcon from "@/assets/icons/back-arrow.svg?react";
+import { TABS } from "@/constant/tabs";
+import { useCurrentTabIndex } from "@/states/tabSlice";
+import { useRef } from "react";
 
 function Header() {
-    const currentTab = useSelector(state => state.tab.currentTab);
-    const isInChatRoom = useSelector(state => state.chat.isInChatRoom);
-    const dispatch = useDispatch();
-
+    const [currentTabIndex] = useCurrentTabIndex();
+    const currentPath = useLocation().pathname;
+    const isTab = currentTabIndex != -1 ? true : false;
+    const currentTab = currentTabIndex != -1 ? TABS[currentTabIndex] : null;
+    const navigate = useNavigate();
+    const prevPath = useRef(currentPath);
+    if (isTab) {
+        prevPath.current = currentPath;
+    }
     const handleBackInChat = () => {
-        dispatch(exitChatRoom());
-        dispatch(setShowNavigator(true));
+        navigate(-1);
     };
 
     return (
         <Nav>
-            {isInChatRoom && <button onClick={handleBackInChat}>뒤로가기</button>}
-            {currentTab}
-            <NavLink to="/"></NavLink>
+            {!isTab && (
+                <>
+                    <BackArrowIcon onClick={handleBackInChat} />
+                    {/* <currentTab.icon /> */}
+                </>
+            )}
+            {currentTab?.name}
         </Nav>
     );
 }
