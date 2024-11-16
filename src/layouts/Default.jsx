d4a887480c51c38d5124fc9519aee31003d8dd00
heebp <skdnfl4343@gmail.com>
2024-11-17 01:24:38 +0900

    refactor: 탭의 인덱스를 상태관리하도록 변경

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -3,19 +3,22 @@ import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { setCurrentTab } from "@/states/tabSlice";
+import { setCurrentTabIndex } from "@/states/tabSlice";
 import Header from "@/components/Header";
 import BottomTab from "@/components/BottomTab";
 import { OutletContainer } from "./Default.styles";
+import { TABS } from "@/constant/tabs";
 
 export default function DefaultLayout() {
     const dispatch = useDispatch();
     const location = useLocation();
+
     const showNavigator = useSelector(state => state.navigator.showNavigator);
+    const targetTabIndex = TABS.findIndex(tab => tab.path === location.pathname);
 
     useEffect(() => {
-        dispatch(setCurrentTab(window.location.pathname));
-    }, [dispatch, location.pathname]);
+        dispatch(setCurrentTabIndex(targetTabIndex));
+    }, [dispatch, targetTabIndex]);
     return (
         <>
             <Header />
