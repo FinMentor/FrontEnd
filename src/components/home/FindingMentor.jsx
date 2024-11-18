2e4155de77062d3ea86ea1ca99f4f6b2ce2c226a
HYUNTAK-KIM <eggu24@gmail.com>
2024-11-18 14:20:05 +0900

    feat: 홈 고수 찾기 CSS 구현

--- src/components/home/FindingMentor.jsx
+++ src/components/home/FindingMentor.jsx
@@ -1,12 +1,65 @@
-// import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
+// eslint-disable-next-line prettier/prettier
+import { MentorContainer, MentorCard, ProfileImage, MentorInfo, MentorCategory, MentorName, MentorReturn } from "./FindingMentor.styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-import { MentorContainer } from "./FindingMentor.styles";
+// 임의의 데이터 예시
+const mentors = [
+    {
+        id: 1,
+        profileImage: "https://image.newsis.com/2024/11/06/NISI20241106_0001696513_web.jpg?rnd=20241106171229",
+        category: "예금/적금",
+        name: "빽이",
+        monthlyReturn: "+7.5%",
+    },
+    {
+        id: 2,
+        profileImage: "https://image.newsis.com/2024/11/06/NISI20241106_0001696513_web.jpg?rnd=20241106171229",
+        category: "주식",
+        name: "태양물고기",
+        monthlyReturn: "+5.2%",
+    },
+    {
+        id: 3,
+        profileImage: "https://example.com/profile3.jpg",
+        category: "부동산",
+        name: "부동산 전문가",
+        monthlyReturn: "+3.8%",
+    },
+    {
+        id: 4,
+        profileImage: "https://example.com/profile4.jpg",
+        category: "채권",
+        name: "채권 분석가",
+        monthlyReturn: "+4.6%",
+    },
+    {
+        id: 5,
+        profileImage: "https://example.com/profile5.jpg",
+        category: "펀드",
+        name: "펀드 매니저",
+        monthlyReturn: "+6.1%",
+    },
+];
 
 function FindingMentor() {
     return (
         <MentorContainer>
             <h2>지금, 내가 찾고 있는 고수들</h2>
+            <Swiper spaceBetween={16} slidesPerView="auto" freeMode={true}>
+                {mentors.map(mentor => (
+                    <SwiperSlide key={mentor.id} style={{ width: "250px" }}>
+                        <MentorCard>
+                            <ProfileImage src={mentor.profileImage} alt={`${mentor.name} 프로필`} />
+                            <MentorInfo>
+                                <MentorCategory>{mentor.category}</MentorCategory>
+                                <MentorName>{mentor.name}</MentorName>
+                                <MentorReturn>{mentor.monthlyReturn}</MentorReturn>
+                            </MentorInfo>
+                        </MentorCard>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </MentorContainer>
     );
 }
--- src/components/home/FindingMentor.styles.jsx
+++ src/components/home/FindingMentor.styles.jsx
@@ -2,4 +2,59 @@ import styled from "@emotion/styled";
 
 export const MentorContainer = styled.div`
     padding: 24px;
+    background-color: #ffffff;
+
+    h2 {
+        margin: 0 0 20px 0;
+        font-family: "Pretendard Bold";
+        font-size: 24px;
+    }
+`;
+
+export const MentorCard = styled.div`
+    background-color: #f7f8f8;
+    border-radius: 15px;
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    text-align: left;
+    width: 100%;
+`;
+
+export const ProfileImage = styled.img`
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    margin-right: 15px;
+`;
+
+export const MentorInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+`;
+
+export const MentorCategory = styled.div`
+    font-size: 12px;
+    color: #949ba0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`;
+
+export const MentorName = styled.div`
+    font-size: 14px;
+    font-weight: bold;
+    margin-top: 4px;
+    display: flex;
+    align-items: center;
+`;
+
+export const MentorReturn = styled.div`
+    font-size: 12px;
+    color: #fd3018;
+    font-weight: bold;
+    margin-top: 4px;
 `;
