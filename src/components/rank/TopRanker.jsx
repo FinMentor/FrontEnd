a45104dc1aa64afb0e23c5cdb2bc3d5a2840e9bd
HYUNTAK-KIM <eggu24@gmail.com>
2024-11-22 06:37:19 +0900

    refactor: 랭크 페이지 CSS 수정

--- src/components/rank/TopRanker.jsx
+++ src/components/rank/TopRanker.jsx
@@ -1,4 +1,13 @@
-import { Root, HintBox, FirstPlace, SecondPlace, ThirdPlace } from "./TopRanker.styles";
+import {
+    Root,
+    HintBox,
+    FirstPlace,
+    SecondPlace,
+    ThirdPlace,
+    ProfileImage,
+    Nickname,
+    Category,
+} from "./TopRanker.styles";
 import alertInfo from "@/assets/icons/alert-info.svg";
 import PropTypes from "prop-types";
 import mainCategory from "@/utils/mainCategory";
@@ -21,12 +30,20 @@ function TopRanker({ rankers }) {
                 랭킹은 어떻게 결정되나요?
             </HintBox>
             <FirstPlace>
-                {rankers?.[0]?.nickname}
-                <img src={rankers?.[0]?.profileImageUrl} alt="profile" />
-                {mainCategory(rankers?.[0]?.mainCategoryId)}
+                <ProfileImage src={rankers?.[0]?.profileImageUrl} alt="profile" />
+                <Nickname>{rankers?.[0]?.nickname}</Nickname>
+                <Category>{mainCategory(rankers?.[0]?.mainCategoryId)}</Category>
             </FirstPlace>
-            <SecondPlace>{rankers?.[1]?.nickname}</SecondPlace>
-            <ThirdPlace>{rankers?.[2]?.nickname}</ThirdPlace>
+            <SecondPlace>
+                <ProfileImage src={rankers?.[1]?.profileImageUrl} alt="profile" />
+                <Nickname>{rankers?.[1]?.nickname}</Nickname>
+                <Category>{mainCategory(rankers?.[1]?.mainCategoryId)}</Category>
+            </SecondPlace>
+            <ThirdPlace>
+                <ProfileImage src={rankers?.[2]?.profileImageUrl} alt="profile" />
+                <Nickname>{rankers?.[2]?.nickname}</Nickname>
+                <Category>{mainCategory(rankers?.[2]?.mainCategoryId)}</Category>
+            </ThirdPlace>
         </Root>
     );
 }
